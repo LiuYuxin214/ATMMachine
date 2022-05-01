9bb7257a59c8e66238ec371d6b22cc9ee2567e9f
LiuYuxin214 <13791114010@163.com>
2022-05-02 03:09:50 +0800

    Liu Yuxin

--- src/AccountMaker.java
+++ src/AccountMaker.java
@@ -5,28 +5,31 @@ import java.util.Scanner;
 public class AccountMaker {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Account[] accounts = new Account[10];
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of the file: ");
-        int n = sc.nextInt();
+        int n = input.nextInt();
         System.out.print("Enter the balance of the file: ");
-        double b = sc.nextDouble();
+        double balance = input.nextDouble();
         System.out.print("Enter the password of the file: ");
-        String p = sc.next();
+        String password = input.next();
         System.out.print("Enter the question of the file: ");
-        int q = sc.nextInt();
+        int question = input.nextInt();
         System.out.print("Enter the answer of the file: ");
-        String a = sc.next();
-        for (int i = 0; i < n; i++) {
-            accounts[i] = new Account(i, b, p);
-            accounts[i].setDateCreate();
-            accounts[i].setQuestion(q);
-            accounts[i].setAnswer(a);
-            accounts[i].saveToFile();
-        }
+        String answer = input.next();
+        Account[] accounts = new Account[n];
         PrintWriter pw = new PrintWriter("AccountList.txt");
-        pw.println(n);
+        for (int i = 0; i < n; i++) {
+            accounts[i] = new Account(i, balance, password);
+            accounts[i].setDateCreate();
+            accounts[i].setQuestion(question);
+            accounts[i].setAnswer(answer);
+            accounts[i].saveToFile();
+            pw.println(accounts[i].getUserID());
+        }
         pw.close();
+        PrintWriter pw2 = new PrintWriter("numOfAccounts.txt");
+        pw2.println(n);
+        pw2.close();
         System.out.println("Accounts created successfully!");
     }
 }
---
+++ src/AdministratorMaker.java
@@ -0,0 +1,26 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class AdministratorMaker {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of the file: ");
+        int n = input.nextInt();
+        System.out.print("Enter the password of the file: ");
+        String password = input.next();
+        Administrator[] a = new Administrator[n];
+        PrintWriter pw = new PrintWriter("AdministratorList.txt");
+        for (int i = 0; i < n; i++) {
+            a[i] = new Administrator(i + "", password);
+            a[i].saveToFile();
+            pw.println(a[i].getUserName());
+        }
+        pw.close();
+        PrintWriter pw2 = new PrintWriter("numOfAdministrators.txt");
+        pw2.println(n);
+        pw2.close();
+        System.out.println("Administrators created successfully!");
+    }
+}

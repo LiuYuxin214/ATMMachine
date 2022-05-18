2d519e044ea89f22e6e6c736872d2e5f6c9328f2
LiuYuxin214 <13791114010@163.com>
2022-05-18 14:44:11 +0800

    Liu Yuxin

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         while (true) {
-            System.out.print("Enter an ID (Type \"s\" to system menu): ");
+            System.out.println("Welcome to ATM!");
+            System.out.println("Type \"s\" to system menu. If you forget your password, type \"f\".");
+            System.out.print("Enter an ID : ");
             Scanner input = new Scanner(System.in);
             String enter = input.nextLine();
             if (enter.matches("\\d+")) {
@@ -21,6 +23,25 @@ public class Main {
                 }
             } else if (enter.equals("s")) {
                 SystemMenu.moreOperation();
+            } else if (enter.equals("f")) {
+                System.out.print("Enter your ID: ");
+                int id = Integer.parseInt(input.nextLine());
+                Account account = new Account(id);
+                account.getFromFile();
+                System.out.println("Your password reset question is: ");
+                System.out.println(account.getQuestion());
+                System.out.print("Enter the answer to your password reset question: ");
+                String answer = input.next();
+                if (answer.equals(account.getAnswer())) {
+                    System.out.print("Enter a new password: ");
+                    String newPassword = input.next();
+                    account.setPassword(newPassword);
+                    account.saveToFile();
+                    System.out.println("Password reset successfully");
+                } else {
+                    System.out.println("Incorrect answer");
+                }
+                Waiter.waiter();
             } else {
                 System.out.println("User not found");
                 Waiter.waiter();

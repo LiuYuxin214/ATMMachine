d2b69c574d782cf9623921f8d2a2d3abfc17495c
LiuYuxin214 <13791114010@163.com>
2022-05-21 16:14:46 +0800

    Liu Yuxin

--- src/AdministratorMaker.java
+++ src/AdministratorMaker.java
@@ -7,11 +7,13 @@ public class AdministratorMaker {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of the file: ");
         int n = input.nextInt();
+        System.out.print("Enter the user name of the file: ");
+        String userName = input.next();
         System.out.print("Enter the password of the file: ");
         String password = input.next();
         Administrator[] a = new Administrator[n];
         for (int i = 0; i < n; i++) {
-            a[i] = new Administrator(i + "", password);
+            a[i] = new Administrator(userName + i, password);
             a[i].saveToFile();
         }
         System.out.println("Administrators created successfully!");
